71cf1b46bea6d9ec64cd462ca838f18d77f6c936
Santiago Gomez <Santiago.lsgo@gmail.com>
2024-08-21 11:54:27 -0500

    explicacion git

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,20 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
+
+modifas el proyecto
+git add .
+
+haces el commit 
+git commit -m "modife tal archivo"
+
+haces el push para que quede subido en git hub
+git push origin alimentacion
+
+para bajar los cambios del repositorio 
+git pull origin alimentacion
 |
-*/
+*
 
 
 //Route::get('/', function () {
@@ -19,4 +31,3 @@ use Illuminate\Support\Facades\Route;
 //});
 
 
-santiago aoisjioaj
