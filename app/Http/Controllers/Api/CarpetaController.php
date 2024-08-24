cb0c6e758b1d158b91aeda156e5ae8f39ccb3b69
andres <andresenar96@gmail.com>
2024-08-24 12:23:26 -0500

    cambio controller ruta api 24-08

--- app/Http/Controllers/Api/CarpetaController.php
+++ app/Http/Controllers/Api/CarpetaController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Carpeta;
-use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 
 class CarpetaController extends Controller
