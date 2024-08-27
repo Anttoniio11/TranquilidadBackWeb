665bf689ba94f12318089ad0064f0f003acdcca3
Daniela Diaz <calidad8844@gmail.com>
2024-08-26 21:08:06 -0500

    m

---
+++ database/migrations/2024_08_26_231655_create_audios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('audios', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('file_path');  // Para almacenar la ruta del archivo de audio
+            $table->integer('duration')->comment('Duration in seconds');
+            $table->foreignId('genre_id')->constrained('genres')->onDelete('cascade');
+            $table->foreignId('album_id')->nullable()->constrained('albums')->onDelete('cascade');
+            $table->foreignId('binaural_sound_id')->nullable()->constrained('binaural_sounds')->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audios');
+    }
+};
---
+++ database/migrations/2024_08_26_231728_create_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name'); // Nombre del tag
+            $table->string('taggable_type'); // Tipo del modelo relacionado (Audio, Podcast, etc.)
+            $table->unsignedBigInteger('taggable_id'); // ID del modelo relacionado
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+    }
+};
