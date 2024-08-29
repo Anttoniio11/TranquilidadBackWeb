fde4b02b08a317e5ca0d53697a466fe8fc24679b
Anttoniio11 <anttoniio2312@gmail.com>
2024-08-29 15:20:12 -0500

    Se realizan cambios en models

--- app/Models/Professional.php
+++ app/Models/Professional.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Http\Resources\CategoryResourse;
 
 class Professional extends Model
 {
